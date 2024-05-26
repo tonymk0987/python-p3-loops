6a4d3785a0599f1faf0f6821106df1a0152bf524
tonymk0987 <k.tony.muriuki@gmail.com>
2024-05-26 23:51:05 +0300

    Complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []  
+    for num in int_list:  
+        squared_list.append(num ** 2)  
+    return squared_list  
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):  # This Loop through numbers from 1 to 100
+        if num % 3 == 0 and num % 5 == 0:  # This will Check if the number is divisible by both 3 and 5
+            print("FizzBuzz")
+        elif num % 3 == 0:  # Check if the number is divisible by 3
+            print("Fizz")
+        elif num % 5 == 0:  # Check if the number is divisible by 5
+            print("Buzz")
+        else:  # If none of the above conditions are met, simply print the number
+            print(num)
+
+
